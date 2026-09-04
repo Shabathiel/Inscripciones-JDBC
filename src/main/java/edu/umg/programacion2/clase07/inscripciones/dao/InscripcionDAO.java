248270880f2c593d382ca7255954f9093647a3f6
Emilyn Verenice Recinos Salguero <erecinoss5@miumg.edu.gt>
2026-09-04 11:47:07 -0600

    feat: listar estudiantes de curso

--- src/main/java/edu/umg/programacion2/clase07/inscripciones/dao/InscripcionDAO.java
+++ src/main/java/edu/umg/programacion2/clase07/inscripciones/dao/InscripcionDAO.java
@@ -166,6 +166,29 @@ public class InscripcionDAO {
         List<Estudiante> resultado = new ArrayList<>();
         // TODO: completar.
 
+        String sql = "SELECT e.id, e.nombre, e.carnet " +
+                "FROM inscripciones i " +
+                "JOIN estudiantes e ON i.estudiante_id = e.id " +
+                "JOIN cursos c ON i.curso_id = c.id " +
+                "WHERE c.nombre = ?";
+        
+        try (Connection conexion = DriverManager.getConnection(URL, USUARIO, PASSWORD);
+        	     PreparedStatement statement = conexion.prepareStatement(sql)) {
+        	
+        	statement.setString(1, nombreCurso);
+        	
+        	try (ResultSet resultadoSQL = statement.executeQuery()) {
+        		 while (resultadoSQL.next()) {
+        			 
+        			 int id = resultadoSQL.getInt("id");
+        			 String nombre = resultadoSQL.getString("nombre");
+        			 String carnet = resultadoSQL.getString("carnet");
+        			 
+        			 resultado.add(new Estudiante(id, nombre, carnet));
+        		 }
+        	}
+        }
+        
         return resultado;
     }
 
